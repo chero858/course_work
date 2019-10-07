0cc75acca9080b30ba8d6d9e6fd8e8898e1f84e8
Roman Cherkasov <chero858@gmail.com>
2019-10-07 17:08:40 +0300

    screen

---
+++ screen.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+
+time.sleep(1)
+pyautogui.screenshot('/home/roman/PycharmProjects/course_work/image.png', region=(480, 320, 395, 395))
+
+
+# time.sleep(3)
+# pyautogui.press('up')
